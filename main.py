2852aa383800555d5f91cc135e2609e8b633ed8a
Marios <marios_2510@hotmail.com>
2023-08-10 15:58:05 +0300

    Allow user input from 2 players. Fully working tic-tac-toe.

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ def display_board(board):
         print(" | ".join(row))
         print("-" * 9)
 
-# Let's create an initial game board and display it
-initial_board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-display_output = display_board(initial_board)
-# Simulating the player's move with the input provided
-player_symbol = 'X'  # Let's assume player X is making the move
-
-def player_move_simulation(board, player_symbol, simulated_input):
+def player_move(board, player_symbol):
     valid_move = False
     while not valid_move:
         try:
-            move = simulated_input  # Using the provided input instead of calling input()
+            move = int(input(f"{player_symbol}'s move (1-9): "))
             if 1 <= move <= 9:
                 row, col = (move - 1) // 3, (move - 1) % 3
                 if board[row][col] not in ['X', 'O']:
@@ -28,27 +22,45 @@ def player_move_simulation(board, player_symbol, simulated_input):
     return board
 
 def check_win(board, player_symbol):
-    # Check rows, columns, and diagonals
     for i in range(3):
         if all([cell == player_symbol for cell in board[i]]) or \
            all([board[j][i] == player_symbol for j in range(3)]):
             return True
-            
+
     if board[0][0] == player_symbol and board[1][1] == player_symbol and board[2][2] == player_symbol:
         return True
     if board[0][2] == player_symbol and board[1][1] == player_symbol and board[2][0] == player_symbol:
         return True
-        
+
     return False
 
 def check_tie(board):
     for row in board:
         for cell in row:
             if cell not in ['X', 'O']:
-                return False  # The board still has empty cells
+                return False
     return True
 
+def tic_tac_toe_interactive():
+    board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+    current_player = 'X'
 
+    for _ in range(9):
+        display_board(board)
+        board = player_move(board, current_player)
 
-updated_board = player_move_simulation(initial_board, player_symbol, 2)
-display_output = display_board(updated_board)
+        if check_win(board, current_player):
+            display_board(board)
+            print(f"{current_player} wins!")
+            return
+        elif check_tie(board):
+            display_board(board)
+            print("It's a tie!")
+            return
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+    print("It's a tie!")
+
+# When you run this in your local environment, call the function to start the game
+tic_tac_toe_interactive()
