9c0cb7c4471be31a84804715e46721c5cfa49729
Marios <marios_2510@hotmail.com>
2023-08-15 17:09:43 +0300

    Add docstrings and comments.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 def display_board(board):
+    """
+    Display the current state of the game board.
+    
+    Parameters:
+    - board (list of lists): The game board represented as a 2D list.
+    """
     for row in board:
         print(" | ".join(row))
-        print("-" * 9)
+        print("-" * (len(board) * 3))
 
 def get_board_size():
+    """
+    Prompt the user to input the desired size for the game board.
+    
+    Returns:
+    - int: The selected size for the board.
+    """
     while True:
         try:
             size = int(input("Enter board size (e.g., 3 for 3x3, 4 for 4x4, etc.): "))
-            if size >= 3:  # Let's assume a minimum size of 3x3 for the game to be meaningful
+            if size >= 3:  # Minimum size of 3x3 for the game
                 return size
             else:
                 print("Please enter a size of 3 or greater.")
@@ -15,10 +27,28 @@ def get_board_size():
             print("Please enter a valid number.")
 
 def create_board(size):
+    """
+    Create a game board of the specified size filled with numbers.
+    
+    Parameters:
+    - size (int): The size of the board.
+    
+    Returns:
+    - list of lists: The created game board.
+    """
     return [[str(i * size + j + 1) for j in range(size)] for i in range(size)]
 
-
 def player_move_dynamic(board, player_symbol):
+    """
+    Get the next move from the current player and update the board.
+    
+    Parameters:
+    - board (list of lists): The current game board.
+    - player_symbol (str): The symbol ('X' or 'O') of the current player.
+    
+    Returns:
+    - list of lists: The updated game board.
+    """
     valid_move = False
     size = len(board)
     while not valid_move:
@@ -37,11 +67,17 @@ def player_move_dynamic(board, player_symbol):
             print(f"Please enter a valid number between 1-{size*size}.")
     return board
 
-# Note: Since this environment is not interactive in the traditional sense, we can't play the game here.
-# However, you can replace your 'player_move' function with the 'player_move_dynamic' function in your code
-# and then run it in your local Python environment to play the game interactively.
-
 def check_win(board, player_symbol):
+    """
+    Check if the current player has won the game.
+    
+    Parameters:
+    - board (list of lists): The current game board.
+    - player_symbol (str): The symbol ('X' or 'O') of the current player.
+    
+    Returns:
+    - bool: True if the player has won, False otherwise.
+    """
     size = len(board)
     
     # Check rows and columns
@@ -60,9 +96,16 @@ def check_win(board, player_symbol):
 
     return False
 
-    return False
-
 def check_tie(board):
+    """
+    Check if the game has ended in a tie.
+    
+    Parameters:
+    - board (list of lists): The current game board.
+    
+    Returns:
+    - bool: True if the game is a tie, False otherwise.
+    """
     for row in board:
         for cell in row:
             if cell not in ['X', 'O']:
@@ -70,6 +113,10 @@ def check_tie(board):
     return True
 
 def tic_tac_toe_interactive():
+    """
+    Run the interactive Tic Tac Toe game. The function handles the game loop, player switches,
+    and checks for game termination conditions.
+    """
     size = get_board_size()
     board = create_board(size)
     current_player = 'X'
@@ -87,7 +134,9 @@ def tic_tac_toe_interactive():
             print("It's a tie!")
             return
 
+        # Switch player
         current_player = 'O' if current_player == 'X' else 'X'
 
-# When you run this in your local environment, call the function to start the game
-tic_tac_toe_interactive()
+# Note for GitHub users: To start the game, uncomment the line below and run the script.
+# tic_tac_toe_interactive()
+
